8455a93f595e3d98a7ce289f65ef39ae3fd8a872
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-07-06 12:45:45 -0400

    views updated

--- slj_backend/shows_api/views.py
+++ slj_backend/shows_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Show
+from .serializers import ShowSerializer
 
 # Create your views here.
+
+
+class ShowList(generics.ListCreateAPIView):
+    queryset = Show.objects.all().order_by('id')
+    serializer_class = ShowSerializer
+
+class ShowDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Show.objects.all().order_by('id')
+    serializer_class = ShowSerializer
