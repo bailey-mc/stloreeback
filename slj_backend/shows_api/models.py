e73056d3219c9538fcafb97eccd1d724c33865d0
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-07-06 12:38:11 -0400

    model created

--- slj_backend/shows_api/models.py
+++ slj_backend/shows_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Show(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank = True)
+    videofile = models.FileField(upload_to='shows/', null=True, verbose_name="")
+
+    def __str__(self):
+        return self.name + ": " + str(self.videofile)
